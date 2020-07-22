d82ab70736bd42b8678bb35820b77a1000175f95
Davide Gironi <davide.gironi@gmail.com>
2020-07-22 20:15:10 +0200

    heroku push

--- pokthello-api/src/index.js
+++ pokthello-api/src/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 // serve static pages
 if (process.env.SERVESTATICPAGES) {
-  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(favicon('public/favicon.ico'));
   app.use(express.static('public'));
 } else {
   // set root route
