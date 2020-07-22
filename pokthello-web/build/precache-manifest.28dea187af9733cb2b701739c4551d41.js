4e9efbd7e11c8d7e81284edd33da93a704ed32a7
Davide Gironi <davide.gironi@gmail.com>
2020-07-22 19:23:34 +0200

    updated gitignore

---
+++ pokthello-web/build/precache-manifest.28dea187af9733cb2b701739c4551d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c73f687fef823a280e2f12519ca6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113c88d2ab236ca6a3ff",
+    "url": "/static/css/main.7b3d2122.chunk.css"
+  },
+  {
+    "revision": "f8eba291577b813da355",
+    "url": "/static/js/2.97bb50c8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.97bb50c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113c88d2ab236ca6a3ff",
+    "url": "/static/js/main.70ae6ea3.chunk.js"
+  },
+  {
+    "revision": "54d2e911e6c6da5a61d0",
+    "url": "/static/js/runtime-main.5f21a124.js"
+  }
+]);
